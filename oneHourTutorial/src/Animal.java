a841d772c1f4607607a6ed4cc86daa66146f564e
B1Z0N <kolausf@gmail.com>
2019-10-05 16:42:55 +0300

    Added getters and setters

--- oneHourTutorial/src/Animal.java
+++ oneHourTutorial/src/Animal.java
@@ -30,6 +30,75 @@ public class Animal {
         System.out.println("5 + 1 = " + minusOfNumbers);
         int productOfNumbers = 5 * 1;
         System.out.println("5 * 1 = " + productOfNumbers);
+
+        System.out.print("Enter name: \n");
+        if (userinput.hasNextLine()) {
+            this.setName(userinput.nextLine());
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean isHasOwner() {
+        return hasOwner;
+    }
+
+    public void setHasOwner(boolean hasOwner) {
+        this.hasOwner = hasOwner;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public long getUniqueId() {
+        return uniqueId;
+    }
+
+    public void setUniqueId(long uniqueId) {
+        this.uniqueId = uniqueId;
+    }
+
+    public char getFavoriteChar() {
+        return favoriteChar;
+    }
+
+    public void setFavoriteChar(char favoriteChar) {
+        this.favoriteChar = favoriteChar;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
     }
 
     public static void main(String[] args) {
